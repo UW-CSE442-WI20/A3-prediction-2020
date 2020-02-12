155301caed016862d157efd11bae36a3e5b2b647
robelw <robelw@cs.washington.edu>
2020-02-12 14:08:38 -0800

    not using the latest csvfile

--- src/index.js
+++ src/index.js
@@ -1,56 +1,86 @@
-const d3 = require('d3');
-const topo = require('topojson');
-
-//var svg = d3.select("#map").append("svg").attr("width", 960).attr("height",600);
-
- const width = window.innerWidth;
- const height = window.innerHeight;
-//projection
-var projection = d3.geoAlbersUsa();/** .translate([width / 2, height / 2])
-                 .scale((width - 1) / 2 / Math.PI);*/
-var pathGenerator = d3.geoPath().projection(projection);
-
-const zoom = d3.zoom()
-             .scaleExtent([1, 8]).on('zoom', zoomed);
-
-const svg = d3.select("#map").append("svg").attr("width", width).attr("height",height);
-const g = svg.append('g');
-svg.call(zoom);
-// load the csv file
-const csvFile = require('./us_counties2016.csv');
-fillMap(csvFile);
-
-//##############################################
- /**
-   * function to fill the map with appropriate background
-   */
-  function fillMap(csvFile) {
-    d3.csv(csvFile).then(function(data) {
-        var winner = [];
-        for (var i = 0; i < data.length; i++) {
-            winner.push({id:parseInt(data[i].Fips),name:data[i].Winner.split(" ")[0]});
-            
+(function() {
+    "use strict";
+      
+    // module global variables
+    const d3 = require('d3');
+    const topo = require('topojson');
+    //projection
+    const projection = d3.geoAlbersUsa();
+    const pathGenerator = d3.geoPath().projection(projection);
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const svg = d3.select("#map").append("svg").attr("width", width).attr("height",height);
+    var winner;
+    // load the csv file
+    //const csvFile_2000 = require('./us_counties2000.csv');
+    //const csvFile_2004 = require('./us_counties2008.csv');
+    //const csvFile_2008 = require('./us_counties2012.csv');
+    //const csvFile_2012 = require('./us_counties2016.csv');
+    var csvFile_2016 = require('./us_counties2016.csv');
+    //
+    window.addEventListener("load",init);
+    function init() {
+        // do this for each year
+        drawStartupScreen();
+        d3.select("#five").on("click", function() {fillMap(csvFile_2016);},false);
+        
+    }
+    
+    /**
+     * show the start up screen
+     */
+    function drawStartupScreen() {
+        // display the map
+        d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json").then(function(json) {
+            const counties = topo.feature(json, json.objects.counties);
+            svg.selectAll('path')
+               .data(counties.features)
+               .enter().append('path')
+               .attr("id", function (d) {return parseInt(d.id);})
+               .attr("class","counties").attr('d', pathGenerator);
+      });
+    
+    }
+    
+    
+    /**const zoom = d3.zoom()
+                 .scaleExtent([1, 8]).on('zoom', zoomed);
+    
+    
+    const g = svg.append('g');
+    svg.call(zoom);
+    
+    //fillMap(csvFile); */
+    
+    //##############################################
+    
+    function fillMap(csvFile) {
+        // not efficient to reconstruct map on every click
+        d3.csv(csvFile).then(function(data) {
+            winner = [];
+            for (var i = 0; i < data.length; i++) {
+                winner.push({id:parseInt(data[i].Fips),name:data[i].Winner.split(" ")[0]});
+                    
         }
+        colorMap(winner);});
+        
+               
+    }
     
-    d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json").then(function(json) {
-        const counties = topo.feature(json, json.objects.counties);
-        g.selectAll('path')
-           .data(counties.features)
-           .enter().append('path')
-           .attr("id", function (d) 
-           {var temp = winner.find(x => parseInt(d.id) == x.id); if (temp) {return temp.name};})
-           .attr("class","counties").attr('d', pathGenerator);/**.on("click",clicked);*/  
-    });
     
-    });
-
-}
-
-//################################################################################//
-
-function zoomed() {
-    g.selectAll('path').attr('transform', d3.event.transform);
-}
-
-
-
+    function colorMap(map) {
+        //console.log(map);
+        for (var i = 0; i < map.length; i++) {
+            console.log(map[i].id);
+            //d3.select("#" + map[i].id).classList.add(map[i].name);
+                
+        }
+    }
+    //################################################################################//
+    function zoomed() {
+        g.selectAll('path').attr('transform', d3.event.transform);
+    }
+    
+    
+    })();
+    
