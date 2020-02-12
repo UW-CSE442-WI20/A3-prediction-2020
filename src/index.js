ee3ee526bd3967c6c3de2d42d5395f68ee3a0edd
robelw <robelw@cs.washington.edu>
2020-02-11 17:04:00 -0800

    not sure what is wrong with Alaska

--- src/index.js
+++ src/index.js
@@ -12,31 +12,18 @@ const csvFile = require('./us_counties2016.csv');
 d3.csv(csvFile).then(function(data) {
     var winner = [];
     for (var i = 0; i < data.length; i++) {
-        //console.log(data[i]);
-        var temp = parseFloat(data[i].percentage);
-        if (temp > 0.5) {
-            //console.log(parseInt(data[i].Fips));
-            //console.log(temp);
-            winner.push({id:parseInt(data[i].Fips),name:data[i].Candidate});
-            //console.log(winner);
-        }
+        winner.push({id:parseInt(data[i].Fips),name:data[i].Winner.split(" ")[0]});
+        
     }
 
-/** });*/
-// #############################################################################################
-// load geoJson data and merge with csvFile
 d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json").then(function(json) {
     const counties = topo.feature(json, json.objects.counties);
-    //console.log(counties.features[0].id);
-
     svg.selectAll('path')
        .data(counties.features)
        .enter().append('path')
        .attr("class", function (d) 
-       {var temp = winner.find(x => parseInt(d.id) == x.id); if (temp) {return temp.name.split(" ")[0]};})
-       .attr('d', pathGenerator);
-
-    
+       {var temp = winner.find(x => parseInt(d.id) == x.id); if (temp) {return temp.name};})
+       .attr('d', pathGenerator);  
 });
 
 });
